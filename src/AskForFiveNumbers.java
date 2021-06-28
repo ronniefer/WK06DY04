65b84d1a8eb383c4eae65f126dede25e49baa20c
ronniefrias <ronnie.frias@gmail.com>
2021-06-28 08:45:08 -0500

    Convert Array to ArrayList

--- src/AskForFiveNumbers.java
+++ src/AskForFiveNumbers.java
@@ -1,42 +1,43 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class AskForFiveNumbers 
 {
 	public static void main(String[] args)
 	{
-		int numArray[] = new int[5];
+		ArrayList<Integer> numArrayList = new ArrayList<Integer>(5);
+		
 		int sum = 0;
 		int product = 1;
 		
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("What is the first number:");
-		numArray[0] = input.nextInt();
+		numArrayList.add(input.nextInt());
 		System.out.println("What is the second number:");
-		numArray[1] = input.nextInt();
+		numArrayList.add(input.nextInt());
 		System.out.println("What is the third number:");
-		numArray[2] = input.nextInt();
+		numArrayList.add(input.nextInt());
 		System.out.println("What is the fourth number:");
-		numArray[3] = input.nextInt();
+		numArrayList.add(input.nextInt());
 		System.out.println("What is the fifth number:");
-		numArray[4] = input.nextInt();
+		numArrayList.add(input.nextInt());
 		
-		for (int i : numArray)
+		for (int i : numArrayList)
 		{
 			sum += i;
 			product *= i;
 		}
 		
-		Arrays.sort(numArray);
-		
-	    int min = numArray[0];
-	    int max = numArray[numArray.length-1];
+	    int min = Collections.min(numArrayList);
+	    int max = Collections.max(numArrayList);
 
-		System.out.println("Sum of numbers in Array is : " + sum);
-		System.out.println("Product of numbers in Array is : " + product);
-		System.out.println("The smallest number in the Array is : " + min);
-		System.out.println("The largest number in the Array is : " + max);
+		System.out.println("Sum of numbers in ArrayList is : " + sum);
+		System.out.println("Product of numbers in ArrayList is : " + product);
+		System.out.println("The smallest number in the ArrayList is : " + min);
+		System.out.println("The largest number in the ArrayList is : " + max);
 	}
 
 }
